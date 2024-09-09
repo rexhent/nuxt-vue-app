48ae3c2f6a2a26fd2c2378ec6c2af5757ed8bfab
Dominic <67210734+rexhent@users.noreply.github.com>
2024-09-10 09:00:00 +1000

    some unocss examples

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -1,6 +1,13 @@
 <template>
     <div flex>
-        <p>Created by me 2024</p>
+    <!-- <div flex="~"> -->
+    <!-- <div class="flex"> -->
+
+        <p color="blue">Created by me 2024</p>
+        <!-- <p color-blue>Created by me 2024</p>
+        <p class="color-blue">Created by me 2024</p> -->
+        <!-- These are all the same-->
+
         <a href="https://github.com/rexhent" target="_blank" rel="noopener noreferrer">Github</a>
         <a>Website</a>
     </div>
